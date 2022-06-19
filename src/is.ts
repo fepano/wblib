6ce5a8f946b16de774f03c45018c1c7d79081cf9
wopen <nepow@foxmail.com>
2022-06-19 17:25:31 +0800

    release: v1.0.1

--- src/is.ts
+++ src/is.ts
@@ -15,3 +15,11 @@ export function isFunction(o: any): o is Function {
 export function isNumber(o: any): o is number {
   return typeof o === 'number' || (toString.call(o) === '[object Number]');
 }
+
+export function isValidNumber(o: any): o is Number {
+  const r = isNumber(o);
+  if (!r) return r;
+
+  // eslint-disable-next-line no-restricted-globals
+  return (!isNaN(o) && isFinite(o));
+}
