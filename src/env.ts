7f99c90a5af3712551998fb2da3a1e90b88556e6
wopen <nepow@foxmail.com>
2022-06-25 17:28:25 +0800

    release: v1.0.5

--- src/env.ts
+++ src/env.ts
@@ -1,2 +1,2 @@
 export const isBrowser = typeof window !== 'undefined';
-export const isMobile = isBrowser && ('ontouchstart' in window || navigator.maxTouchPoints > 0);
+export const isTouch = isBrowser && ('ontouchstart' in window || navigator.maxTouchPoints > 0);
